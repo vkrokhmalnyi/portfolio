64ca9ae9b0a358e19e7c959cf77a27a1275089e2
Volodymyr <vkrokhmalniy@gmail.com>
2016-06-13 08:35:03 +0000

    add new file

--- js/classie.js
+++ js/classie.js
@@ -1,15 +1,21 @@
-/*!
- * classie - class helper functions
- * from bonzo https://github.com/ded/bonzo
- * 
- * classie.has( elem, 'my-class' ) -> true/false
- * classie.add( elem, 'my-new-class' )
- * classie.remove( elem, 'my-unwanted-class' )
- * classie.toggle( elem, 'my-class' )
- */
 
-/*jshint browser: true, strict: true, undef: true */
-/*global define: false */
+$('#btn-send').click(function(e) {
+  $.ajax({
+      url: "https://formspree.io/s.krokhmalnyi@gmail.com",  
+      method: "POST",
+      error: function () {
+        $('#message').html('<h1>Error!</h1>');
+      },
+      data: {
+             email: email.value,
+             message: textMessage.value
+             },
+      dataType: "json"
+  }).done(function() {
+     $('#message').html('<h1>Thank you!</h1><h1>Your message has been sent</h1>');
+  });
+  e.preventDefault();
+});
 
 ( function( window ) {
 
